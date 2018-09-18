cdd311e9d76dc0007211ad835e6be55109396ee8
Árni Fannar <arnis18@ru.is>
2018-09-18 17:47:31 +0000

    Fyrsta tilraun tölvuleiks.

---
+++ TileTraveller.py
@@ -0,0 +1,63 @@
+room = 11
+while room == 11:
+    direction = input("You can travel (N)orth.")
+    if direction = 'n':
+        room += 1
+    else:
+        print('Not a valid direction!')
+while room == 12:
+    direction = input("You can travel (N)orth or (E)ast or (S)outh.")
+    if direction = 'n':
+        room += 1
+    elif direction = 'e':
+        room += 10
+    elif direction = 's':
+        room -= 1
+    else:
+        print('Not a valid direction!')
+while room == 13:
+    direction = input("You can travel (E)ast or (S)outh.")
+    if direction = 'e':
+        room += 10
+    elif direction = 's':
+        room -= 1
+    else:
+        print('Not a valid direction!')
+while room == 23:
+    direction = input("You can travel (W)est or (E)ast.")
+    if direction = 'w':
+        room -= 10
+    elif direction = 'e':
+        room += 10
+    else:
+        print('Not a valid direction!')
+while room == 33:
+    direction = input("You can travel (W)est or (S)outh.")
+    if direction = 'w':
+        room -= 10
+    elif direction = 's':
+        room -= 1
+    else:
+        print('Not a valid direction!')
+while room == 32:
+    direction = input("You can travel (N)orth or (S)outh.")
+    if direction = 'n':
+        room += 1
+    elif direction = 's':
+        room -= 1
+    else:
+        print('Not a valid direction!')
+while room == 22:
+    direction = input("You can travel (W)est or (S)outh.")
+    if direction = 'w':
+        room -= 10
+    elif direction = 's':
+        room -= 1
+    else:
+        print('Not a valid direction!')
+if room == 31:
+    print("Victory!")
+
+    
+        
+
