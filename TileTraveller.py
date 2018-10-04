adc375c31ba6f936c17ce67e55d6ce9ce315342d
Árni Fannar <arnis18@ru.is>
2018-10-04 18:23:14 +0000

    Saving solution.

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,65 +1,107 @@
+#https://github.com/arnis18/TileTraveller
+
 room = 11
+try_n = 0
 while room != 31:
     while room == 11:
-        direction = input("You can travel (N)orth.\n Direction: ")
-        if direction == 'n':
+        if try_n == 0:
+            print("You can travel (N)orth.")
+        direction = input("Direction: ")
+        try_n +=1
+        if direction.lower() == 'n':
             room += 1
+            try_n = 0
         else:
             print('Not a valid direction!')
     while room == 12:
-        direction = input("You can travel (N)orth or (E)ast or (S)outh.\n Direction: ")
-        if direction == 'n':
+        if try_n == 0:
+            print("You can travel (N)orth or (E)ast or (S)outh")
+        direction = input("Direction: ")
+        try_n +=1
+        if direction.lower() == 'n':
             room += 1
-        elif direction == 'e':
+            try_n = 0
+        elif direction.lower() == 'e':
             room += 10
-        elif direction == 's':
+            try_n = 0
+        elif direction.lower() == 's':
             room -= 1
+            try_n = 0
         else:
             print('Not a valid direction!')
     while room == 13:
-        direction = input("You can travel (E)ast or (S)outh.\n Direction: ")
-        if direction == 'e':
+        if try_n == 0:
+            print("You can travel (E)ast or (S)outh.")
+        direction = input("Direction: ")
+        try_n +=1
+        if direction.lower() == 'e':
             room += 10
-        elif direction == 's':
+            try_n = 0
+        elif direction.lower() == 's':
             room -= 1
+            try_n = 0
         else:
             print('Not a valid direction!')
     while room == 23:
-        direction = input("You can travel (W)est or (E)ast.\n Direction: ")
-        if direction == 'w':
+        if try_n == 0:
+            print("You can travel (W)est or (E)ast.")
+        direction = input("Direction: ")
+        try_n +=1
+        if direction.lower() == 'w':
             room -= 10
-        elif direction == 'e':
+            try_n = 0
+        elif direction.lower() == 'e':
             room += 10
+            try_n = 0
         else:
             print('Not a valid direction!')
     while room == 33:
-        direction = input("You can travel (W)est or (S)outh.\n Direction: ")
-        if direction == 'w':
+        if try_n == 0:
+            print("You can travel (W)est or (S)outh.")
+        direction = input("Direction: ")
+        try_n +=1
+        if direction.lower() == 'w':
             room -= 10
-        elif direction == 's':
+            try_n = 0
+        elif direction.lower() == 's':
             room -= 1
+            try_n = 0
         else:
             print('Not a valid direction!')
     while room == 32:
-        direction = input("You can travel (N)orth or (S)outh.\n Direction: ")
-        if direction == 'n':
+        if try_n == 0:
+            print("You can travel (N)orth or (S)outh.")
+        direction = input("Direction: ")
+        try_n +=1
+        if direction.lower() == 'n':
             room += 1
-        elif direction == 's':
+            try_n = 0
+        elif direction.lower() == 's':
             room -= 1
+            try_n = 0
         else:
             print('Not a valid direction!')
     while room == 22:
-        direction = input("You can travel (W)est or (S)outh.\n Direction: ")
-        if direction == 'w':
+        if try_n == 0:
+            print("You can travel (W)est or (S)outh.")
+        direction = input("Direction: ")
+        try_n +=1
+        if direction.lower() == 'w':
             room -= 10
+            try_n = 0
         elif direction == 's':
             room -= 1
+            try_n = 0
         else:
             print('Not a valid direction!')
     while room == 21:
-        direction = input("You can travel (N)orth.\n Direction: ")
-        if direction == 'n':
+        if try_n == 0:
+            print("You can travel (N)orth.")
+        direction = input("Direction: ")
+        try_n +=1
+        if direction.lower() == 'n':
             room += 1
+            try_n = 0
         else:
             print('Not a valid direction!')
 else:
